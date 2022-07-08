64d3564bf7297726d30527bc0c031a93f3e63508
lmw7414 <lmw7414@naver.com>
2022-07-08 17:45:14 +0900

    .

--- src/main/java/com/gdsc/cookieparking/cookieparking/controller/UserController.java
+++ src/main/java/com/gdsc/cookieparking/cookieparking/controller/UserController.java
@@ -4,9 +4,7 @@ import com.gdsc.cookieparking.cookieparking.domain.User;
 import com.gdsc.cookieparking.cookieparking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -16,6 +14,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
 
     @PostMapping("/users")
     public ResponseEntity<?> create(@RequestBody User resource) throws URISyntaxException {
@@ -29,7 +28,11 @@ public class UserController {
         User user = userService.registerUser(id, name, email, password, confirmPassword);
         String url = "/users/" + user.getId();
         return ResponseEntity.created(new URI(url)).body("{}");
+    }
 
-
+    @DeleteMapping("/users/{id}")
+    public String delete(@PathVariable String id) {
+        //TODO : 회원삭제 구현 기능 필요
+        return null;
     }
 }
